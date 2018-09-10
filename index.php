d8a5269d02aa18cfe818e994bfde8fd6ccae4f42
Kevin Pruus <h1ghl4nd3r94@users.noreply.github.com>
2018-09-10 10:41:38 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Minu esimene leht lol</title>
+</head>
+<body>
+<h1>Teretulemast minu veebisaidile</h1>
+<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames. Siin hakkavad siis asetsema minu Kevin Pruusi dokumendid ja tekstid.
+Olen töö- ja tehnologiaõppeppe kolmanda kursuse tudeng ning plaanin õigeigeaegselt lõpetada</p>
+<p>Minu sõps teeb ka siin veebiproge <a href="../../~rogemik">veebi</a>. </p>
+<h2>Antud hetkel rohkem nagu lisada pole...</h2>
+<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ terra hoone">-->
+<!--<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ terra hoone">-->
+<img src=
+"https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/27867214_1807132252652671_1766114752174874559_n.jpg?_nc_cat=0&oh=52fd1692c8c06bd653e8a88ad0035711&oe=5BF174D2" alt="Pilt minu jalgrattast">
+
+</body>
+
+</html>
